03dc0e4cd226b8ba5b62b78206976039a0e3a31d
mohammadasif2808 <mohammadasif2808@gmail.com>
2024-01-26 10:44:24 +0530

    celcius to fahrenheit

--- src/main/java/in/mohammadasif/temperature/Temperature.java
+++ src/main/java/in/mohammadasif/temperature/Temperature.java
@@ -1,16 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package in.mohammadasif.temperature;
 
 /**
  *
  * @author mohammad asif
+ * Email: mohammadasif2808@gmail.com
+ * Date: Jan 26, 2024
  */
 public class Temperature {
     
-    public static void main(String[] args) {
-        System.out.println("Temperature");
+    public static int celciusToFahrenheit(int temp) {
+        int fTemp = (( 9 * temp) /5) + 32;
+        return fTemp;
     }
+    
+    public static void examineTemperature(int temp){
+        System.out.println("\nTemperature is "+ temp + " Celcius. Hmm....");
+        
+        int fTemp = celciusToFahrenheit(temp);
+        
+        System.out.println(".... that's "+ fTemp + " Fahrenheit");
+        
+        if (fTemp < 45) {
+            System.out.println("That's chilly");
+        } else if (fTemp > 80) {
+            System.out.println("That's pretty warm");
+        } else {
+            System.out.println("That's nothing special");
+        }
+    }
+    
+    public static void main(String[] args) {
+        int[] temperatures = {-10, 5, 25, 38};
+        
+        for(int i=0; i < temperatures.length; i++) {
+            examineTemperature(temperatures[i]);
+        }
+    }
+
 }
